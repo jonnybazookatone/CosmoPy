b2a70227a8870e1feeae6cfb00282da5feb36326
Jonny Elliott <jonnynelliott@googlemail.com>
2014-08-18 20:43:50 +0100

    Allows test and training data to be included. A bunch of other improvements included

--- photoz.py
+++ photoz.py
@@ -15,7 +15,7 @@ import time, argparse, logging
 
 class PhotoSample(object):
 
-  def __init__(self, filename):
+  def __init__(self, filename_train=False, filename_test=False, filename=False):
 
     # Setup the logger to the command line
     # To a file can also be added fairly easily
@@ -27,21 +27,18 @@ class PhotoSample(object):
     ch = logging.StreamHandler() #console handler
     ch.setFormatter(formatter)
     logger.addHandler(ch)
-
     self.logger = logger
 
-    # Check they give a filename
-    if not filename:
-      self.logger.warning("You must give a filename")
-      sys.exit(0)
-      
+    # Book keeping of what the user entered
+    self.filename_train = filename_train
+    self.filename_test = filename_test
     self.filename = filename
 
     # PCA analysis
     self.num_components = 5
     self.test_size = 10000
-    self.data_frame = None
-    self.data_frame_header = ["up","gp","rp","ip","zp"]
+    
+  
     self.PCA_data_frame = None
     self.data_frame_test = None
     self.data_frame_train = None
@@ -53,14 +50,43 @@ class PhotoSample(object):
     # Plots
     self.lims = {"x": [0.3, 0.8], "y": [0.41, 0.71]}
 
-  def load_data_frame(self):
+
+    # This is for more test purposes
+    if self.filename:
+      self.data_frame = self.load_data_frame(filename)
+      self.logger.info("You gave a complete file, seperating training sets")
+
+    # This is for normal users
+    elif self.filename_test and filename_train:
+      self.data_frame_test = self.load_data_frame(filename_test)
+      self.data_frame_train = self.load_data_frame(filename_train)
+
+
+      self.data_frame = self.data_frame_test.copy()
+      self.data_frame = self.data_frame.append(self.data_frame_train)
+
+    else:
+      self.logger.warning("You must give a training and test set or a complete file.")
+      sys.exit(0)
+
+  def load_data_frame(self, filename):
     try:
-      self.data_frame = pd.read_csv(self.filename, encoding="utf-8")
+      data_frame = pd.read_csv(filename, encoding="utf-8")
+
+
+      self.data_frame_header = [i for i in data_frame.columns if i not in ["redshift", "specObjID"]]
+
+      # rows = np.random.choice(data_frame.index.values, 20000)
+      # sampled_df = data_frame.ix[rows]
+
+      return data_frame
+      # return sampled_df
+
     except:
       self.logger.info("Failed to open CSV file: {0}".format(sys.exc_info()[0]))
+      sys.exit(0)
 
   def do_PCA(self):
-
     # TODO:
     # Do the following on the fly:
     #   1. determine num_components
@@ -70,6 +96,9 @@ class PhotoSample(object):
     from sklearn.decomposition import PCA
     self.logger.info("Carrying out Principle Component Analysis ({0} components)".format(self.num_components))
     pca = PCA(self.num_components)
+
+    print self.data_frame[self.data_frame_header].shape
+
     pca.fit(self.data_frame[self.data_frame_header])
     M_pca = pca.fit_transform(self.data_frame)
 
@@ -79,17 +108,24 @@ class PhotoSample(object):
     for i in range(self.num_components):
       M_df["PC{0:d}".format(i+1)] = M_pca[:,i]
 
+
     self.PCA_data_frame = pd.DataFrame(M_df)
 
-  def split_sample(self):
+  def split_sample(self, random):
 
-    from sklearn.cross_validation import train_test_split
     # Cross Validation Section
     ##
     self.logger.info("Splitting into training/testing sets. Number of testing: {0}".format(self.test_size))
     ## Split into train/test
       
-    train, test = train_test_split(self.PCA_data_frame, test_size=int(self.test_size), random_state=42)
+    if random:
+      from sklearn.cross_validation import train_test_split
+      train, test = train_test_split(self.PCA_data_frame, test_size=int(self.test_size), random_state=42)
+    else:
+      left = self.data_frame_train.shape[0]
+
+      train = self.PCA_data_frame[:left]
+      test = self.PCA_data_frame[left:]
 
     self.logger.info("Training set length: {0}".format(train.shape[0]))
     self.logger.info("Testing set length: {0}".format(test.shape[0]))
@@ -97,15 +133,25 @@ class PhotoSample(object):
     ## Redefine some DataFrames, otherwise they are just numpy arrays
     col_train = {}
     col_test = {}
-    col_train["redshift"] = train[:,-1]
-    col_test["redshift"] = test[:,-1]
+    try:
+      col_train["redshift"] = train[:,-1]
+      col_test["redshift"] = test[:,-1]
+      for i in range(self.num_components):
+        col_train["PC{0:d}".format(i+1)] = train[:,i]
+        col_test["PC{0:d}".format(i+1)] = test[:,i]
 
-    for i in range(self.num_components):
-      col_train["PC{0:d}".format(i+1)] = train[:,i]
-      col_test["PC{0:d}".format(i+1)] = test[:,i]
+    except:
+      col_train["redshift"] = train["redshift"].values
+      col_test["redshift"] = test["redshift"].values
+      for i in range(self.num_components):
+        col_train["PC{0:d}".format(i+1)] = train["PC{0:d}".format(i+1)]
+        col_test["PC{0:d}".format(i+1)] = test["PC{0:d}".format(i+1)]
+
+
 
     self.data_frame_test = pd.DataFrame(col_test)
     self.data_frame_train = pd.DataFrame(col_train)
+
 
   def do_GLM(self):
 
@@ -156,7 +202,7 @@ class PhotoSample(object):
     ax.set_ylabel(r"$\rm Density$", fontsize=20)
     ax.set_position([.15,.15,.75,.75])
 
-    plt.savefig("KDE_PLOT_{0}.pdf".format(self.filename.split("/")[-1]), format="pdf")
+    plt.savefig("KDE_PLOT_{0}.pdf".format("test"), format="pdf")
     plt.clf()
 
     g = sns.JointGrid(self.measured, self.predicted,size=4,space=0)
@@ -174,7 +220,7 @@ class PhotoSample(object):
     axx.set_position([.15, .85, .7, .13])
     axy.set_position([.88, .12, .13, .7])
 
-    plt.savefig("KDE_2D_PLOT_{0}.pdf".format(self.filename.split("/")[-1]), format="pdf")
+    plt.savefig("KDE_2D_PLOT_{0}.pdf".format("test"), format="pdf")
     if show:
       plt.show()
 
@@ -182,19 +228,28 @@ class PhotoSample(object):
     self.logger.info("Everything is ok")
 
   def run_full(self, show=False):
-    self.load_data_frame()
     self.do_PCA()
-    self.split_sample()
+
+    if self.filename:
+      random = True
+    else:
+      random = False
+
+    self.split_sample(random=random)
     self.do_GLM()
     self.make_plot(show=show)
 
 
-
 def main():
 
-  TWOSLAQ = PhotoSample(filename="../data/2slaq.csv")
-  TWOSLAQ.print_info()
-  TWOSLAQ.run_full(show=True)
+  # TWOSLAQ = PhotoSample(filename="../data/2slaq.csv")
+  # TWOSLAQ.print_info()
+  # TWOSLAQ.run_full(show=True)
+
+  SDSS = PhotoSample(filename_train="../data/SDSS_train.csv", filename_test="../data/SDSS_test.csv")
+  # SDSS = PhotoSample(filename="../data/SDSS_ntrai.csv")
+  SDSS.print_info()
+  SDSS.run_full(show=True)
 
 
 if __name__=='__main__':
